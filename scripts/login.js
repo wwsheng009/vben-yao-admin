ad82922bddbc64015448b639d944f04d9f682a4f
wwsheng009 <vincentwwsheng@gmail.com>
2023-04-24 21:15:54 +0800

    update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,12 @@
-
 // 用户登录payload,email或是mobile
 // {
 //     "captcha": {
 //         "code": "27819",
 //         "id": "dOA980tYva9kSBgJtqVm"
 //     },
-//     "email": "test@qq.com" 
+//     "email": "test@qq.com"
 //     "mobile": "18038068070",
-//     "password": "wwsheng",
+//     "password": "123456",
 //     "sid": "LBwKVt1iwT6EMx-Vgp9CX1675945319386"
 // }
 /**
@@ -16,60 +15,58 @@
  * @returns
  */
 function User(payload) {
-    let captcha = Process("yao.utils.CaptchaValidate", payload.captcha.id,
-        payload.captcha.code)
-    if (captcha !== true) {
-        throw new Exception("验证码不正确!", 400);
-    }
-    var account = payload.mobile;
-    if (!account) {
-        account = payload.email;
-    }
-    var user = Process("models.contact.user.get", {
-        wheres: [
-            { column: "mobile", value: account },
-            { column: "status", value: "启用" },
-            {
-                method: "orwhere",
-                column: "email",
-                value: account,
-            },
-        ],
-        limit: 1,
-    });
-    if (!user || !user.length) {
-        throw new Exception("用户不存在", 400);
-    }
-    // 验证密码
-    var password_validate = Process(
-        "utils.pwd.Verify",
-        payload.password,
-        user[0].password
-    );
-    if (password_validate !== true) {
-        throw new Exception("密码不正确!", 400);
-    }
+  const captcha = Process(
+    "yao.utils.CaptchaValidate",
+    payload.captcha.id,
+    payload.captcha.code
+  );
+  if (captcha !== true) {
+    throw new Exception("验证码不正确!", 400);
+  }
 
-    // 增加token等信息
-    var session_id = Process("session.start");
-    var jwt = Process(
-        "utils.jwt.Make",
-        user[0].id,
-        { user_name: user[0].name },
-        {
-            timeout: 28800,
-            sid: session_id,
-        }
-    );
-    Process("session.set", "user", user[0], 28800);
-    Process("session.set", "token", jwt.token, 28800);
-    Process("session.set", "user_id", user[0].id, 28800);
-    delete user[0].password;
-    return {
-        sid: session_id,
-        user: user[0],
-        menus: Process("flows.app.menu"),
-        token: jwt.token,
-        expires_at: jwt.expires_at,
-    };
+  let account = payload.mobile || payload.email;
+
+  const [user] = Process("models.contact.user.get", {
+    wheres: [
+      { column: "mobile", value: account },
+      { column: "status", value: "启用" },
+      { method: "orwhere", column: "email", value: account },
+    ],
+    limit: 1,
+  });
+
+  if (!user) {
+    throw new Exception("用户不存在", 400);
+  }
+
+  const password_validate = Process(
+    "utils.pwd.Verify",
+    payload.password,
+    user.password
+  );
+
+  if (password_validate !== true) {
+    throw new Exception("密码不正确!", 400);
+  }
+
+  const sessionId = Process("session.start");
+  let userPayload = { ...user };
+  delete userPayload.password;
+  const jwtOptions = {
+    timeout: 28800,
+    sid: sessionId,
+  };
+  const jwtClaims = { user_name: user.name };
+  const jwt = Process("utils.jwt.Make", user.id, jwtClaims, jwtOptions);
+  Process("session.set", "user", userPayload, 28800);
+  Process("session.set", "token", jwt.token, 28800);
+  Process("session.set", "user_id", user.id, 28800);
+
+  return {
+    sid: sessionId,
+    user: userPayload,
+    menus: Process("flows.app.menu"),
+    token: jwt.token,
+    expires_at: jwt.expires_at,
+  };
 }
